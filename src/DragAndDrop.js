5fe3dd6dd654c2f167936c48ca613cd1b01cced0
TosinMary <teetoastl3@gmail.com>
2023-09-20 22:55:40 +0100

    fixed drag and drop issue

--- src/DragAndDrop.js
+++ src/DragAndDrop.js
@@ -1,21 +1,44 @@
-import React from 'react'
-// import { Card} from 'react-bootstrap';
-import './styles/drag.css'
+import React, { useState } from 'react';
 
+function DragAndDrop(props) {
+  const [isDragging, setIsDragging] = useState(false);
 
- function DragAndDrop (props) {
+  const handleDragStart = (e) => {
+    setIsDragging(true);
+    e.dataTransfer.setData('text/plain', props.index);
+  };
+
+  const handleDragEnd = () => {
+    setIsDragging(false);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const fromIndex = parseInt(e.dataTransfer.getData('text/plain'), 10);
+    const toIndex = props.index;
+
+    props.onDrop(fromIndex, toIndex);
+  };
 
   return (
-    
-     
-     <div  className='m-5' id='
-     cardContainer' >
-  <img src={props.image} alt='img' className='img-fluid' />
-  <h3>{props.title}</h3>
-</div>
-
-  )
+    <div
+      className={`draggable ${isDragging ? 'dragging' : ''}`}
+      draggable='true'
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+    >
+      <div style={{ marginTop: '2em' }}>
+        <img src={props.image} alt='img' className='img-fluid' />
+        <h3>{props.title}</h3>
+      </div>
+    </div>
+  );
 }
 
-
-export default DragAndDrop
+export default DragAndDrop;
--- src/component/DragIt.js
+++ src/component/DragIt.js
@@ -1,48 +1,54 @@
-import React from 'react';
-import { useState } from 'react';
-import DragAndDrop from '../DragAndDrop';
-import {BiSearchAlt2, mBiSearchAlt2 } from "react-icons/bi"
+import React, { useState } from 'react';
+import DragAndDrop from '../DragAndDrop'; 
+import '../styles/drag.css'
+import {BiSearchAlt2 } from "react-icons/bi"
+
 
 const data = [
-    {
-        id: 1,
-        image: "./Images/fruits1.jpg",
-        title: "Banana"
-    },
-    {
-        id: 2,
-        image: "./Images/fruit2.jpg",
-        title: "Pawpaw"
-    },
-    {
-        id: 4,
-        image: "./Images/fruit4.jpg",
-        title: "Watermelon"
-    },
-    {
-        id: 5,
-        image: "./Images/fruit5.jpg",
-        title: "lemon"
-    },
-    {
-        id: 6,
-        image: "./Images/fruit6.jpg",
-        title: "Strawberry"
-    },
-    {
-        id: 7,
-        image: "./Images/fruit7.jpg",
-        title: "Apple"
-    },
-   
-
-]
+  {
+    id: 1,
+    image: './Images/fruits1.jpg',
+    title: 'Banana',
+  },
+  {
+    id: 2,
+    image: './Images/fruit2.jpg',
+    title: 'Pawpaw',
+  },
+  {
+    id: 4,
+    image: './Images/fruit4.jpg',
+    title: 'Watermelon',
+  },
+  {
+    id: 5,
+    image: './Images/fruit5.jpg',
+    title: 'lemon',
+  },
+  {
+    id: 6,
+    image: './Images/fruit6.jpg',
+    title: 'Strawberry',
+  },
+  {
+    id: 7,
+    image: './Images/fruit7.jpg',
+    title: 'Apple',
+  },
+];
 
 const DragIt = () => {
-const  [images, setImages] = useState(data)
+  const [images, setImages] = useState(data);
 
+  const handleDrop = (fromIndex, toIndex) => {
+    const updatedImages = [...images];
+    const [draggedImage] = updatedImages.splice(fromIndex, 1);
+    updatedImages.splice(toIndex, 0, draggedImage);
+    setImages(updatedImages);
+  };
 
-const handleOnChange = (e) => {
+  
+  const handleOnChange = (e) => {
     const userInput = e.target.value.toLowerCase();
   
     const filteredData = data.filter((item) =>
@@ -51,24 +57,26 @@ const handleOnChange = (e) => {
   
     setImages(filteredData);
   };
-  
+
+
   return (
-    <div className='container' style={{padding:'2em'}}>
-
-    <div className='flex w-[30%] bg-black text-white rounded-lg mx-auto'>
-        <input type='text'  className=' px-4  w-full outline-none bg-transparent ' onChange={handleOnChange} placeholder='Search for fruits'></input>
+    <div className='container' style={{ padding: '2em' }}>
+      <div className='flex w-[30%] bg-black text-white rounded-lg mx-auto'>
+        <input
+          type='text'
+          className='px-4 w-full outline-none bg-transparent'
+          placeholder='Search for fruits' onChange={handleOnChange}
+        />
         <BiSearchAlt2 className='w-18 h-8 text-white' />
+      </div>
+
+      <div style={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row', gap: '2em' }}>
+        {images.map((item, index) => (
+          <DragAndDrop key={item.id} image={item.image} title={item.title} index={index} onDrop={handleDrop} />
+        ))}
+      </div>
     </div>
-
-   
-    <div style={{display:'flex', flexWrap:'wrap' ,flexDirection:'row', gap:'2em'}}>
-        {images.map(items => 
-             <DragAndDrop key={items.id} id={items.id} image={items.image} title={items.title}/>)}
-  </div>
-   </div>
   );
-
-
-}
+};
 
 export default DragIt;
